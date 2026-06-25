b3b245fb5a1ad802440c383119786c9d0de2d733
Miguel Ferreira Fernandes <xmiguelrjx@gmail.com>
2026-06-25 18:39:28 -0300

    Add files via upload

---
+++ respondi uma pergunta codificando.py
@@ -0,0 +1,8 @@
+conhecido = "menor"
+desconhecido = "aperto_de_mão"
+
+input("a pessoa é um conhecido? ")
+if input == "sim":
+    print(f"fala ae {conhecido}!")
+else:
+    print(f"tudo bom? *{desconhecido}*")
